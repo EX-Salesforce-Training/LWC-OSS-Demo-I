5da4298ce9da5c2e24f2750f6d2edc65355627c2
harveyhill <43756739+undefined@users.noreply.github.com>
2021-11-12 16:49:29 +0000

    Push from Demo-I

--- src/demoOne.js
+++ src/demoOne.js
@@ -3,7 +3,7 @@ import { api, LightningElement, track, wire } from 'lwc';
 export default class DemoOne extends LightningElement {
 
 
-    greetingOne = 'Bonjour World';
+    greetingOne = 10;
     
     @api
     greetingTwo = 'Hola World';
