17c279553b45ab28403a4e9187634129712eac29
harveyhill <43756739+undefined@users.noreply.github.com>
2021-11-12 16:50:37 +0000

    Push from Demo-VI

---
+++ src/carousel.js
@@ -0,0 +1,5 @@
+import { LightningElement } from "lwc";
+
+export default class carousel extends LightningElement {
+ picture1 = "https://th.bing.com/th/id/OIP.f4x2n3JmRO2ShADsGoS4OQHaHa?pid=ImgDet&rs=1"
+}
